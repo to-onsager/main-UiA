063fc9c58675d9bb5f8e6be204f7e69464d218fa
Tobias Onsager <t.onsager@gmail.com>
2024-10-25 21:51:58 +0000

    Update tablecontent.mjs

--- tablecontent.mjs
+++ tablecontent.mjs
@@ -1,12 +1,23 @@
 const CONTENT_TABLE = {
-no: { title: "Vil du spille Stein, Saks, Papir?"}
-
-
-en: { title: "Would you like to play a game of Rock, Paper, Scissors?"
-
-
-
-}
-
-
- }
+    no: { title: "Velkommen. Vil du spille en runde Stein, Saks, Papir?",
+          startGameYes: "Stein Saks Papir?",
+          rock: "Stein",
+          paper: "Papir",
+          scissor: "Saks"
+    
+        },
+    
+    eng: { title: "Welcome. Would you like to play a game of Rock, Paper, Scissors?",
+         startGameYes: "Rock, Paper or Scissor?.",
+         rock: "Rock",
+         paper: "Paper",
+         scissor: "Scissor"
+    
+    
+        },
+    
+    
+     };
+    
+     export {CONTENT_TABLE}
+    
