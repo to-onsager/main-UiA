0527e4f12402998e2bfb0aa96e107df7d73eac78
Tobias Onsager <t.onsager@gmail.com>
2024-10-25 21:49:07 +0000

    Add files via upload

---
+++ SSP.js
@@ -0,0 +1,4 @@
+import CONTENT_TABLE from './tablecontent.mjs';
+
+console.log(${CONTENT_TABLE.no.title});
+
---
+++ tablecontent.mjs
@@ -0,0 +1,12 @@
+const CONTENT_TABLE = {
+no: { title: "Vil du spille Stein, Saks, Papir?"}
+
+
+en: { title: "Would you like to play a game of Rock, Paper, Scissors?"
+
+
+
+}
+
+
+ }
