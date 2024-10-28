1c572f947ce15fcf69d47827a5c104efd23e09ba
Tobias Onsager <t.onsager@gmail.com>
2024-10-28 01:17:09 +0000

    Update tablecontent.mjs

--- tablecontent.mjs
+++ tablecontent.mjs
@@ -1,23 +1,34 @@
 const CONTENT_TABLE = {
-    no: { title: "Velkommen. Vil du spille en runde Stein, Saks, Papir?",
-          startGameYes: "Stein Saks Papir?",
-          rock: "Stein",
-          paper: "Papir",
-          scissor: "Saks"
-    
-        },
-    
-    eng: { title: "Welcome. Would you like to play a game of Rock, Paper, Scissors?",
-         startGameYes: "Rock, Paper or Scissor?.",
-         rock: "Rock",
-         paper: "Paper",
-         scissor: "Scissor"
-    
-    
-        },
-    
-    
-     };
-    
-     export {CONTENT_TABLE}
-    
+  no: { title: "Vil du spille en runde Stein, Saks, Papir?",
+        promptGameStart: "Trykk enter for å starte spillet!",
+        gameAccept: "Ok, la oss spille!",
+        promptQuestion: "Stein, Saks eller Papir?",
+        rock: "stein",
+        paper: "papir",
+        scissor: "saks",
+        player: "Spilleren",
+        playerRival: "Motstanderen",
+        playerChoice: "Spilleren valgte",
+        RivalChoice: "Motstander valgte",
+  
+      },
+  
+  en: { title: "Do you wish to play a game of Rock, Paper, Scissor?",
+        promptGameStart: "Press return key to start the game!",
+        gameAccept: "Ok, lets start the game",
+        promptQuestion: "Stein, Saks eller Papir?",
+        rock: "rock",
+        paper: "paper",
+        scissor: "scissor",
+        player: "Player",
+        playerRival: "Rival",
+        playerChoice: "Player chose",
+        RivalChoice: "Rival chose",
+  
+      },
+  
+  
+   };
+  
+   export {CONTENT_TABLE};
+  
