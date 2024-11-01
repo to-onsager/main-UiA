483ed56ee3595c21b9d62f07940fbf9f57235126
Tobias Onsager <t.onsager@gmail.com>
2024-11-01 00:33:02 +0000

    dictionary for rockpaperscissor

---
+++ dictionary.mjs
@@ -0,0 +1,25 @@
+const GAME_DICTIONARY = {
+    player1: "du",
+    player2: "AI overloard",
+    draw: "Det ble uavgjort",
+    winner: "Vinneren er : ",
+    selectionQuestion: "Gjør valget ditt (S)tein, S(a)ks,(P)apir",
+    youPicked: "Du valgte",
+    aiPicked: "AI valgte",
+    rock: "S",
+    paper: "P",
+    scissors: "A",
+    choices: ["Stein", "Papir", "Saks"],
+    title: "Velkommen. Vil du spille en runde Stein, Saks, Papir?",
+    promptGameStart: "Tast [y] for ja, tast [n] for å avslutte... ",
+    yesToStart: "Ok, la oss begynne!",
+    noToStart: "Hejdå 🐔",
+    wrongInput: "Ukjent valg, prøv igjen.",
+    preferredLanguage: "Vil du spille på engelsk eller norsk? skriv (EN) for engelsk, (NO) for nosk.",
+    wrongLanguageInput: "Skriv EN eller NO",
+
+
+
+};
+
+export default GAME_DICTIONARY;
