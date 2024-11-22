4689b547aeea9449239bffbeffd89c9fa9a7e2b2
Tobias Onsager <t.onsager@gmail.com>
2024-11-22 00:07:49 +0000

    Added splashscreen for hangman project

---
+++ splashscreen.js
@@ -0,0 +1,37 @@
+let SPLASH = `
+
+                   zzzzzzzzzzzzzzz                   
+                   AAAAAAAAAAAAAAAA                  
+                   AA            AA                  
+                   AA            AA                  
+                   AA         yAAAAAAA               
+                   AA        AAA    AAA              
+                   AA       AAA      BAA             
+                   AA       AAA      BAA             
+                   AA        AAA    AAA              
+                   AA         hAAAAAAB               
+                   AA            AA                  
+                   AA            AA                  
+                   AA       yAAAAAAAAAAC             
+                   AA            AA                  
+                   AA           AAAA                 
+                   AA         AAAABAAA               
+                   AA       AAAA    AAAA             
+                   AA      AAA        DAA            
+                   AA                                
+                   AA                                
+                   AA                                
+            AAAAAAAAAAAAAAAB                         
+             zzzzzzzzzzzzzz            
+       *___ ___*                                        
+       |   Y   .---.-.-----.-----.--------.---.-.-----.
+       |.  1   |  _  |     |  _  |        |  _  |     |
+       |.  _   |___._|__|__|___  |__|__|__|___._|__|__|
+       |:  |   |           |_____|                     
+       |::.|:. |                                       
+       *--- ---*     
+
+`
+
+export { SPLASH };
+
