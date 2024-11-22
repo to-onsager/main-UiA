e42cb8e788fac6da1d5249ab7855f3bd9113c5ee
Tobias Onsager <t.onsager@gmail.com>
2024-11-22 18:18:40 +0000

    Tekst er nå flyttet inn i dictionary fra game.mjs

--- dictionary.mjs
+++ dictionary.mjs
@@ -1,7 +1,14 @@
 const dictionary = {
     wrongGuesses: "Feil gjetninger: ",
     guessPrompt: "Gjett en bokstav eller ord: ",
-    winCelibration: "Hurra du gjettet ordet"
+    winCelibration: "Hurra du gjettet ordet",
+    youAreWrong: "DU TAR FEIL !!!!!!!",
+    youDied: "Du er død",
+    PlayGame: "○◜▏ Vil du spille Hangman? (y/n)",
+    NoPlayGame: "Ok, farvel.",
+    wrongInput: "Ikke gjenkjent valg, skriv (y) for ja eller (n) for å avslutte"
+
 }
+
 
 export default dictionary
