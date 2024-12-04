7115374760461de261e2a39c8c02b7da500b2712
Tobias Onsager <t.onsager@gmail.com>
2024-12-04 19:32:31 +0000

    flyttet feil

--- Tic-Tac-Toe/dictionary.mjs
+++
@@ -1,7 +0,0 @@
-const dictionary = {
-    wrongGuesses: "Feil gjetninger: ",
-    guessPrompt: "Gjett en bokstav eller ord: ",
-    winCelibration: "Hurra du gjettet ordet"
-}
-
-export default dictionary
